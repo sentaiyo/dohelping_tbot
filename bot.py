1837fb4320c0981e52522135cc35b14035e157dd
ilyadr2 <71603061+ilyadr2@users.noreply.github.com>
2020-12-06 20:17:17 +0300

    щощощощ

--- bot.py
+++ bot.py
@@ -1,24 +1,68 @@
-import telebot  # importing part of the pyTelegramBotAPI library
+import telebot
+import config
+import random
+
 from telebot import types
 
-with open("TOKEN.txt") as file:  # takes TOKEN code from TOKEN.txt file
-    TOKEN = str(file.readline())
+bot = telebot.TeleBot(config.TOKEN)
 
 
-bot = telebot.TeleBot(TOKEN)  # defining bot TOKEN
+@bot.message_handler(commands=['start'])
+def welcome(message):
 
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, f"Привет {message.from_user.first_name}! 💫 меня зовут todo bot и я умный помошник, "
-                          f"который поможет тебе быть продуктивнее.\nЕсли хочешь начать процесс создания новой цели 📈,"
-                          f" пришли ок")
+
+    # keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton("🪐ок🪐")
-    markup.add(button1)
+    item1 = types.KeyboardButton("🎲 Рандомное число")
+    item2 = types.KeyboardButton("😊 Как дела?")
 
-'''@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)'''
+    markup.add(item1, item2)
 
-bot.polling()
+    bot.send_message(message.chat.id,
+                     "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(
+                         message.from_user, bot.get_me()),
+                     parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.chat.type == 'private':
+        if message.text == '🎲 Рандомное число':
+            bot.send_message(message.chat.id, str(random.randint(0, 100)))
+        elif message.text == '😊 Как дела?':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'good':
+                bot.send_message(call.message.chat.id, 'Вот и отличненько 😊')
+            elif call.data == 'bad':
+                bot.send_message(call.message.chat.id, 'Бывает 😢')
+
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="😊 Как дела?",
+                                  reply_markup=None)
+
+            # show alert
+            bot.answer_callback_query(callback_query_id=call.id, show_alert=False,
+                                      text="ЭТО ТЕСТОВОЕ УВЕДОМЛЕНИЕ!!11")
+
+    except Exception as e:
+        print(repr(e))
+
+
+# RUN
+bot.polling(none_stop=True)
