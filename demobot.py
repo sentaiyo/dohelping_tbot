ce78419afc40b76f4acc354bf415985961a31597
ilyadr2 <71603061+ilyadr2@users.noreply.github.com>
2020-12-17 11:58:57 +0300

    buttons added

--- demobot.py
+++ demobot.py
@@ -4,8 +4,8 @@ from database import UsersData
 import schedule
 from threading import Thread
 from time import sleep
+from telebot import types
 
-# from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 difficulty = None
@@ -20,20 +20,17 @@ def start_replier(message):
                      "тебе быть продуктивнее".format(
                          message.from_user, bot.get_me()),
                      parse_mode='html')
-    bot.send_message(message.chat.id, "/add - добавить новую задачу\n/del - удалить задачу\n/list - список всех задач\n/set_time - создать уведомление")
+    send_menu(message)
     users_data = UsersData(config.table_path)
     users_data.add_user(message.from_user.id)
 
 
+def send_menu(message):
+    bot.send_message(message.chat.id, "/add - добавить новую задачу\n/del - удалить задачу\n/list - список всех задач\n/set_time - создать уведомление")
+
+
 @bot.message_handler(commands=["add"])
 def ask_difficulty(message):
-    bot.send_message(message.chat.id, "отправь, пожалуйста, сложность своей задачи от 1 до 3")
-    bot.register_next_step_handler(message, get_difficulty)
-
-
-def get_difficulty(message):
-    global difficulty
-    difficulty = message.text  # добавляем сложность новой задачи
     bot.send_message(message.chat.id, "отправь, пожалуйста, текстовым сообщением новую задачу")
     bot.register_next_step_handler(message, get_task)
 
@@ -44,14 +41,49 @@ def get_task(message):
     task = message.text
     users_data = UsersData(config.table_path)
     users_data.add_task(task, difficulty, message.from_user.id)  # добавляем task в бд к пользователю message.chat.id
-    set_time(message)
+
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item1 = types.InlineKeyboardButton("Простая", callback_data='1')
+    item2 = types.InlineKeyboardButton("Не очень", callback_data='2')
+    item3 = types.InlineKeyboardButton("Сложая", callback_data='3')
+
+    markup.add(item1, item2, item3)
+
+    bot.send_message(message.chat.id, "отправь, пожалуйста, сложность своей задачи", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def get_difficulty(call):
+    try:
+        if call.message:
+            if call.data == '1':
+                print(" ")
+            elif call.data == '2':
+                print(" ")
+            elif call.data == "3":
+                print(" ")
+            # remove inline buttons
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="отправь, пожалуйста, сложность своей задачи",
+                                  reply_markup=None)
+            set_time(call)
+    except Exception as e:
+        print(repr(e))
+
+"""def get_difficulty(message):
+    global difficulty
+    difficulty = message.text  # добавляем сложность новой задачи
+    bot.send_message(message.chat.id, "отправь, пожалуйста, текстовым сообщением новую задачу")
+    bot.register_next_step_handler(message, get_task)"""
+
+
+
 
 
 @bot.message_handler(commands=["set_time"])
-def set_time(message):
-    bot.send_message(message.from_user.id, "Укажи время, когда ты свободен\n"
+def set_time(call):
+    bot.send_message(call.message.chat.id, "Укажи время, когда ты свободен\n"
                                            "Например, утром, перед работой/учёбой или вечером после основных дел")
-    bot.register_next_step_handler(message, add_new_user)
+    bot.register_next_step_handler(call.message, add_new_user)
 
 
 def add_new_user(message):
@@ -62,9 +94,7 @@ def add_new_user(message):
     users_data = UsersData(config.table_path)
     users_data.add_time(time, user_id)
     bot.reply_to(message, "Готово")
-    bot.send_message(message.chat.id,
-                     "/add - добавить новую задачу\n/del - удалить задачу\n/list - список всех задач\n/set_time - создать уведомление")
-
+    send_menu(message)
     Thread(target=schedule_checker(time)).start()
 
 
@@ -86,7 +116,7 @@ def send_wakeup_message():
 def del_task(message):
     bot.send_message(message.from_user.id,
                      f'Список задач:\n{get_tasks_list(message.from_user.id)}')
-    bot.send_message(message.chat.id, "отправь задачу, которую нужно удалить")
+    bot.send_message(message.chat.id, "отправь, пожалуйста, текстовым сообщением задачу, которую нужно удалить")
     bot.register_next_step_handler(message, remove_task_from_data_base)
 
 
@@ -98,15 +128,12 @@ def remove_task_from_data_base(message):
     bot.send_message(message.from_user.id,
                      f'Готово, теперь список:\n{get_tasks_list(message.from_user.id)}')  # удаляем task из бд пользователя message.chat.id
 
-    bot.send_message(message.chat.id,
-                     "/add - добавить новую задачу\n/del - удалить задачу\n/list - список всех задач\n/set_time - создать уведомление")
-
+    send_menu(message)
 
 @bot.message_handler(commands=["list"])
 def list_tasks(message):
     bot.send_message(message.from_user.id, get_tasks_list(message.from_user.id))  # в list cписок всех задач пользователя
-    bot.send_message(message.chat.id,
-                     "/add - добавить новую задачу\n/del - удалить задачу\n/list - список всех задач\n/set_time - создать уведомление")
+    send_menu(message)
 
 
 def get_tasks_list(user_id):
@@ -118,7 +145,7 @@ def get_tasks_list(user_id):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
-
+    send_menu(message)
 
 if __name__ == '__main__':
      bot.infinity_polling()
