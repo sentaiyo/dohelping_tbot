b23d38625fb7bf796eea06100c439bb1b8f95973
ilyadr2 <71603061+ilyadr2@users.noreply.github.com>
2020-12-27 20:57:27 +0300

    config changed1

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ TOKEN = "1396445648:AAFWkvKS54NCx7XnUadymL5qeilaMlWYGsc"
 #TOKEN = '1417240849:AAGxJXkPhoVVXch_dnbmWeuhSgKmmKYyy4w'
 #table_path = '/Users/iladruzinin/PycharmProjects/away.php?to=https:/github.com/sentaiyo/away.php?to=https:/github.com/sentaiyo/dohelping_tbot/database.sqlite'
 #table_path = 'C:/Users/Admin/tables/table1.sqlite'
-table_path = "/dohelping_tbot"
+table_path = "/dohelping_tbot/table.sqlite"
