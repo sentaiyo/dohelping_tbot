81f23af9f41be118c434080459a7e957fc07c16e
ilyadr2 <71603061+ilyadr2@users.noreply.github.com>
2020-12-28 09:54:38 +0300

    comments1

--- demobot.py
+++ demobot.py
@@ -155,7 +155,7 @@ def get_tasks_list(user_id):
 
 @bot.message_handler(func=lambda message: True) # bot answers messages that it doesnt understand
 def echo_all(message):
-    bot.reply_to(message, "я тебя не понимаю((")
+    bot.reply_to(message, "я тебя не понимаю(((")
     send_menu(message)
 
 
